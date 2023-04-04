f72af9f5b3ccfe77d2cf934b722694fd520b5ae7
5ag5 <agonzalez555.gg@gmail.com>
2023-04-04 13:40:39 -0500

    Modificaciones de prueba para GitHub

--- homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -16,6 +16,7 @@ public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class);
+		System.out.println("Holas");
 	}
 
 	@Bean
